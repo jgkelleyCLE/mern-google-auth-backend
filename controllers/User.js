e9bc489dc69bcb6c692e2f6d4604e1812f4ceb63
Jack <74841896+jgkelleyCLE@users.noreply.github.com>
2023-12-01 22:51:35 -0500

    update profile route

--- controllers/User.js
+++ controllers/User.js
@@ -1,1 +1,40 @@
 import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+
+export const updateUser = async(req, res) => {
+
+    const userId = req.user.id
+    console.log(userId)
+
+    if(userId !== req.params.id){
+        return res.status(400).json({ message: "Not authorized to update" })
+    }
+
+    try {
+        if(req.body.password){
+            req.body.password = await bcrypt.hash(req.body.password, 10)
+        }
+
+        // const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        //     $set: {
+        //         username: req.body.username,
+        //         email: req.body.email,
+        //         password: req.body.password,
+        //         profilePicture: req.body.profilePicture,
+                
+        //     }
+        // }, { new: true })
+
+        const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true }) 
+
+
+        res.status(200).json(updatedUser)
+        
+        // const { password, ...others } = updatedUser._doc
+        // res.status(200).json(others)
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+}
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import { auth } from '../middleware/auth.js'
+import { updateUser } from '../controllers/User.js'
 
 const router = express.Router()
 
-
+router.put('/update/:id', auth, updateUser)
 
 export default router
